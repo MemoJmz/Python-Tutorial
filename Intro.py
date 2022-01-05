87a9b1047a33ada7edc89178df3b945ec541e825
MemoJmz <mastergajf@gmail.com>
2022-01-04 22:51:35 -0600

    Integers & Floats - Working with Numeric Data

--- Intro.py
+++ Intro.py
@@ -1,60 +1,61 @@
-# Print Welcome Message
+# Integers and FLoats - Working with Numeric Data
 
-# String
-message = 'Hello World'
+# Variables
+num = 3
+num2 = 3.14
 
-print(message)
+# Return the data type
+print(type(num))
+print(type(num2))
 
-# We can know the lenght of the String
-print(len(message))
-# We can also acces to each character
-print(message[0])
-# Or part of the string
-print(message[0:5])
-print(message[6:])
-# Message in lower
-print(message.lower())
-# The count of strings "l" in message
-print(message.count('l'))
+# Arithmetic Operators: +, -, *, /, Foor Division //, Exponent **, Modulus %
 
-# Return the index position of the string in message
-print(message.find('llo'))
+# The fisrt four are well know for us
+print("Floor Division 3//2 =", 3//2)
+print("Exponent 3**2 = ", 3**2)
+print("Modulus 3%2 = ", 3%2)
 
-# Replace Method, return a new string, it needs two parameters
-message2 = message.replace('World', 'Universe')
+# Another examples about modulus
+print(3%2)
+print(4%2)
+print(5%2)
 
-print(message)
-print(message2)
+# Order of operations
+print("3*2+1 = ", 3*2+1)
 
-# Concatenate strings
-greeting = 'Hello'
-name = 'Michael'
-lastname = 'Jimenez'
+# Incrementing a Variable
+num = 1
+num += 1
+# or num = num + 1
+# num *= 10
+print(num)
 
-message = greeting + ', ' + name + ' ' + lastname + '. Welcome!'
-print(message)
+# Methods
+print(f"|-4| = {abs(-4)}")
+print(f"Round of 0.463552 = {round(0.463552)}") # You can choose the number of digits with a second parameter
+print(f"Round of 0.463552 = {round(0.463552, 3)}")
 
-# For the last case it is better use a formatted strings
-message = '{}, {} {}. Welcome!'.format(greeting, name, lastname)
-print(message)
+# Comparison Operators: ==, !=, >, <, >=, <=
+# These kind of operators return a boolean value True or False
 
-# If we are using Python 3.6 or above we can use f strings
-message = f'{greeting}, {name} {lastname}. Welcome!'
-print(message)
+var_1 = 0.54
+var_2 = 5.67
 
-# Something more amazing is we can use methods inside the placeholder
-message = f'{greeting}, {name.lower()} {lastname.upper()}. Welcome!'
-print(message)
+print(f"var_1 == var_2 ? {var_1 == var_2}")
+print(f"var_1 != var_2 ? {var_1 != var_2}")
+print(f"var_1 > var_2 ? {var_1 > var_2}")
+print(f"var_1 < var_2 ? {var_1 < var_2}")
 
-# dir function
-# If we use this function it will show us all of the attributes and methods that
-# we have access
-print(dir(name))
-# or print(dir(str))
+# Note the next
+num_1 = '100'
+num_2 = '200'
+print(f"num_1 = {num_1}")
+print(f"num_2 = {num_2}")
 
-# After seeing all of the methods and attibutes available to this variable
-# we notices that we can get more information about its use
-# For that reason exist the help function
-# print(help(str))
-# for an exact definition
-print(help(str.lower))
+print(f"num_1 + num_2 = {num_1 + num_2}")
+# Here the operator + is not the normal additions with numbers
+# Here it is a concatenation of strings
+# So you need maybe do a cast
+num_1 = int(num_1)
+num_2 = int(num_2)
+print(f"num_1 + num_2 = {num_1 + num_2}")
